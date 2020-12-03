d7d1703914a62bf15a26d7284646023803b0c48d
simentesempre <federico.portoghese@gmail.com>
2020-12-03 17:39:50 +0100

    Sets absolute paths

--- src/unity/index.js
+++ src/unity/index.js
@@ -1,8 +1,10 @@
 import { UnityContent } from "react-unity-webgl"
 
+console.log('paths', `./unity/Build/${process.env.REACT_APP_UNITY_JSON}`,`./unity/Build/${process.env.REACT_APP_UNITY_LOADER}`)
+
 const unityContent  = new UnityContent(
-    `${process.env.PUBLIC_URL}/unity/Build/${process.env.REACT_APP_UNITY_JSON}`,
-    `${process.env.PUBLIC_URL}/unity/Build/${process.env.REACT_APP_UNITY_LOADER}`
+    `./unity/Build/${process.env.REACT_APP_UNITY_JSON}`,
+    `./unity/Build/${process.env.REACT_APP_UNITY_LOADER}`
 )
 
 export default unityContent
