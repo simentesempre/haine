49bf45b8263cc2233c3f7848e985e0679839299e
simentesempre <federico.portoghese@gmail.com>
2020-12-03 17:25:23 +0100

    Fixes public url

--- src/unity/index.js
+++ src/unity/index.js
@@ -2,8 +2,8 @@ import { UnityContent } from "react-unity-webgl"
 
 console.log(process.env.REACT_APP_UNITY_JSON, process.env.REACT_APP_UNITY_LOADER)
 const unityContent  = new UnityContent(
-    `unity/Build/${process.env.REACT_APP_UNITY_JSON}`,
-    `unity/Build/${process.env.REACT_APP_UNITY_LOADER}`
+    `${process.env.PUBLIC_URL}/unity/Build/${process.env.REACT_APP_UNITY_JSON}`,
+    `${process.env.PUBLIC_URL}/unity/Build/${process.env.REACT_APP_UNITY_LOADER}`
 )
 
 export default unityContent
